05a07079944fade59668e6e7ac4d5dabcc9e3791
= <bendev4977@gmail.com>
2023-03-10 04:14:31 +0100

    update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function admin(): Response
     {
-        return $this->render('admin/index.html.twig');
+        return $this->render('pages/admin/admin.html.twig');
     }
 }
--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    #[Route('/user/profil', name: 'app_profil')]
+    public function profil(): Response
     {
-        return $this->render('profil/index.html.twig');
+        return $this->render('pages/profil/profil.html.twig');
     }
 }
