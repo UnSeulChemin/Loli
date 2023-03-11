26f1fce4213174396ba2af73173732354f35dc4f
= <bendev4977@gmail.com>
2023-03-11 17:43:22 +0100

    update

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Contact;
-
 use App\Repository\UserRepository;
 use App\Repository\ContactRepository;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,12 +29,12 @@ class AdminController extends AbstractController
     }
 
     /**
-     * Pagr Admin, Read Users
+     * Page Admin, Read Users
      *
      * @param UserRepository $repository
      * @return Response
      */
-    #[Route('/admin/users', name: 'app_admin_users')]
+    #[Route('/admin/users', name: 'app_admin_users', methods: ['GET'])]
     public function readUsers(UserRepository $repository): Response
     {
         $users = $repository->findBy([], ['id' => 'DESC']);
@@ -42,7 +42,14 @@ class AdminController extends AbstractController
         return $this->render('pages/admin/users.html.twig', compact('users'));
     }
 
-    #[Route('/admin/users/delete/{id}', name: 'app_admin_users_delete')]
+    /**
+     * Page Admin, Delete Users
+     *
+     * @param User $user
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */    
+    #[Route('/admin/users/delete/{id}', name: 'app_admin_users_delete', methods: ['GET'])]
     public function deleteUsers(User $user, EntityManagerInterface $manager): Response
     {
         $manager->remove($user);
@@ -52,23 +59,34 @@ class AdminController extends AbstractController
         return $this->redirectToRoute('app_admin_users');
     }
 
-    
-    #[Route('/admin/read', name: 'app_contact_read', methods: ['GET'])]
-    public function contactRead(ContactRepository $repository): Response
+    /**
+     * Page Admin, Read Contacts
+     *
+     * @param ContactRepository $repository
+     * @return Response
+     */    
+    #[Route('/admin/contacts', name: 'app_admin_contacts', methods: ['GET'])]
+    public function readContacts(ContactRepository $repository): Response
     {
         $contacts = $repository->findBy([], ['id' => 'DESC']);
 
-        return $this->render('pages/contact/contact_read.html.twig', compact('contacts'));
+        return $this->render('pages/admin/contacts.html.twig', compact('contacts'));
     }
 
-    #[Route('/admin/read/delete/{id}', name: 'app_contact_delete', methods: ['GET'])]
-    public function contactDelete(Contact $contact, EntityManagerInterface $manager): Response
+    /**
+     * Page Admin, Delete Contacts
+     *
+     * @param Contact $contact
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/admin/contacts/delete/{id}', name: 'app_admin_contacts_delete', methods: ['GET'])]
+    public function deleteContacts(Contact $contact, EntityManagerInterface $manager): Response
     {
         $manager->remove($contact);
         $manager->flush();
 
         $this->addFlash('success', 'The contact have been successfully delete !');
-        return $this->redirectToRoute('app_contact_read');
+        return $this->redirectToRoute('app_admin_contacts');
     }
-
 }
